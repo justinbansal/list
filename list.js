25060382d15aefda5805d7177348f1d84d4243d9
Justin Bansal <justin.bansal@pixelunion.net>
2024-06-01 00:35:17 -0400

    append results to a container

--- list.js
+++ list.js
@@ -1,15 +1,57 @@
 const searchForm = document.getElementById('search-form');
 const searchInput = document.getElementById('search-bar');
+const resultsContainer = document.querySelector('[data-search-results]');
 
 let query;
 
-searchForm.addEventListener('submit', (e) => {
+// Placeholder content for testing
+
+const results = [
+  { title: 'Bad Boys', poster_path: 'https://placehold.co/400'},
+  { title: 'Bad Boys', poster_path: 'https://placehold.co/400'},
+  { title: 'Bad Boys', poster_path: 'https://placehold.co/400'},
+  { title: 'Bad Boys', poster_path: 'https://placehold.co/400'},
+  { title: 'Bad Boys', poster_path: 'https://placehold.co/400'},
+  { title: 'Bad Boys', poster_path: 'https://placehold.co/400'},
+  { title: 'Bad Boys', poster_path: 'https://placehold.co/400'},
+]
+
+updateResultsContainer(results);
+
+searchForm.addEventListener('submit', async (e) => {
   e.preventDefault();
 
   query = encodeURIComponent(searchInput.value);
 
-  searchMovieDatabase(query);
+  const { results } = await searchMovieDatabase(query);
+
+  if (results.length > 0) {
+    updateResultsContainer(results);
+  }
 });
+
+function updateResultsContainer(results) {
+  const fragment = new DocumentFragment();
+  const baseUrl = `https://image.tmdb.org/t/p/w500/`;
+
+  for (const result of results) {
+    const div = document.createElement('div');
+    div.classList.add('result');
+    const title = document.createElement('h3');
+    title.classList.add('title');
+    title.textContent = result.title;
+    const posterImg = document.createElement('img');
+    posterImg.classList.add('poster');
+    // Removed baseUrl while testing with placeholder content
+    posterImg.src = `${result.poster_path}`;
+    div.append(title);
+    div.append(posterImg);
+    fragment.append(div);
+  }
+
+  resultsContainer.append(fragment);
+
+}
 
 async function searchMovieDatabase(movie) {
   console.log(movie);
@@ -24,7 +66,7 @@ async function searchMovieDatabase(movie) {
   try {
     const response = await fetch(`https://api.themoviedb.org/3/search/movie?query=${movie}&include_adult=false&language=en-US&page=1`, options);
     const data = await response.json();
-
+    return data;
   } catch (error) {
     console.error(`Error fetching data, ${error}`);
   }
